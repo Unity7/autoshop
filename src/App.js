696f19ccba10ac519ff396c5e81a0d1471fef512
Bay Bui <baybui@ucla.edu>
2022-09-04 12:49:28 -0700

    added color selector feature

--- src/App.js
+++ src/App.js
@@ -120,8 +120,33 @@ const Bulb = (props) => {
 };
 
 function App() {
+  const handleClick = (e) => {
+    if (!window.activeMesh) return;
+
+    window.activeMesh.material.color = new THREE.Color(
+      e.target.style.background
+    );
+  };
+
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
+      {/* Color Selector */}
+      <div style={{ position: "absolute", zIndex: 1 }}>
+        <div
+          onClick={handleClick}
+          style={{ background: "blue", height: 50, width: 50 }}
+        ></div>
+        <div
+          onClick={handleClick}
+          style={{ background: "yellow", height: 50, width: 50 }}
+        ></div>
+        <div
+          onClick={handleClick}
+          style={{ background: "white", height: 50, width: 50 }}
+        ></div>
+      </div>
+
+      {/* End of Color Selector */}
       <Canvas
         shadows
         style={{ background: "black" }}
