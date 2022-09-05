881019a6909adebdf2d112c58081c5cc7ae43919
Bay Bui <baybui@ucla.edu>
2022-09-05 11:23:41 -0700

    added bounding boxes

--- src/components/Model.js
+++ src/components/Model.js
@@ -4,7 +4,7 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 const Model = (props) => {
   const model = useLoader(GLTFLoader, props.path);
   console.log(model);
-  return <primitive object={model.scene} {...props} />;
+  return <primitive object={model.scene} scale={props.scale} />;
 };
 
 export default Model;
