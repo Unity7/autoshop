d5ef9c1f52de147e1558bc9d79da4ad08bf21153
Bay Bui <baybui@ucla.edu>
2022-09-05 12:27:05 -0700

    added camera controller

--- src/App.js
+++ src/App.js
@@ -10,14 +10,13 @@ import { Suspense } from "react";
 import { useEffect } from "react";
 import { Physics } from "@react-three/cannon";
 import Orbit from "./components/Orbit";
-import Box from "./components/Box";
 import Background from "./components/Background";
 import Floor from "./components/Floor";
 import Bulb from "./components/Bulb";
 import ColorPicker from "./components/ColorPicker";
-import Dragable from "./components/Dragable";
-import Model from "./components/Model";
-import BoundingBox from "./components/BoundingBox";
+import Cars from "./components/Cars";
+import CameraControls from "./components/CameraControls";
+import CamControls from "./components/CamControls";
 
 function App() {
   return (
@@ -26,46 +25,19 @@ function App() {
       <Canvas
         shadows
         style={{ background: "black" }}
-        camera={{ position: [3, 3, 3] }}
+        camera={{ position: [7, 7, 7] }}
       >
+        {/* <CameraControls /> */}
+        <CamControls />
         {/* <fog attach="fog" args={["white", 1, 10]} /> */}
         <ambientLight intensity={0.2} />
 
         <Orbit attach="orbitControls" />
+
         <axesHelper args={[5]} />
         <Bulb position={[0, 3, 0]} />
         <Physics>
-          <Suspense fallback={null}>
-            {/* transformGroup enables single draggable group object (each model must be wrapped in <Draggable>*/}
-            <Dragable transformGroup>
-              <BoundingBox
-                visible
-                position={[4, 4, 0]}
-                dims={[3, 2, 6]}
-                offset={[0, -0.4, 0.8]}
-              >
-                <Model
-                  path="/tesla_model_3/scene.gltf"
-                  scale={new Array(3).fill(0.01)}
-                  // position={[4, 2, 0]}
-                />
-              </BoundingBox>
-            </Dragable>
-            <Dragable transformGroup>
-              <BoundingBox
-                visible
-                position={[-4, 4, 0]}
-                dims={[3, 2, 6]}
-                offset={[0, -0.8, 0.2]}
-              >
-                <Model
-                  path="/tesla_model_s/scene.gltf"
-                  scale={new Array(3).fill(0.012)}
-                  // position={[-4, 2, 0]}
-                />
-              </BoundingBox>
-            </Dragable>
-          </Suspense>
+          <Cars />
           <Suspense fallback={null}>
             <Background />
           </Suspense>
